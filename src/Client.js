aee6a91a365c8f6b13948f2f34b4d49278c3d435
Hvemvilhvad <malthewbirch@gmail.com>
2026-03-13 15:06:40 +0100

    somnn

--- src/Client.js
+++ src/Client.js
@@ -11,39 +11,27 @@ class TCPClient extends React.Component
 
     render(){
         console.log("fuck yall");
-        this.ConnectToServer();
         return <h1>HI FUCKERS</h1>
     }
 
 
-    ConnectToServer(){
-        // Source - https://stackoverflow.com/a/48709691
-        // Posted by peteb, modified by community. See post 'Timeline' for change history
-        // Retrieved 2026-03-13, License - CC BY-SA 3.0
-
-        // const {Socket} = require('net')
-
-        // const client = new WebSocket('http://192.168.87.152:12000')
-        // const client = new Socket()
-
-        // client.send("sup fucker, wha's good??")
-        // client.connect('192.168.87.152:12000', () => {
-        // console.log('connected to server')
-
-        // client.on('data', message => {
-        //     if (message === 'disconnect') {
-        //     console.log('disconnecting from localhost')
-        //     client.end()
-        //     } else {
-        //     console.log(`Message from the Server: ${message}`)
-        //     }
-        // })
-
-        // client.write('hello!')
-        // })
+    static ConnectToServer(){
+        const client = new WebSocket('http://192.168.87.152:12000')
+        
+        // client.addEventListener("open", event => {
+        //     console.log("connection open, sending name");
+        //     client.send("~username~ webstuff");
+        // });
+        
+        client.addEventListener("message", event => {
+            console.log("Message from server: ", event.data)
+        })
 
     }
 }
+
+
+
 
 
 export default TCPClient;
