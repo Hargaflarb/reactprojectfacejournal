353880f10dd81533085abc93e5380432e89c2076
Hvemvilhvad <malthewbirch@gmail.com>
2026-03-24 15:08:18 +0100

    likes work now

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ class App extends React.Component{
       allPosts:[
         // TemplatePost(), TemplatePost(),TemplatePost(),TemplatePost(),TemplatePost()
       ],
+      postInteractions:[
+
+      ],
       client: props.client.ReferanceExchange(this)
     }
 
@@ -18,7 +21,8 @@ class App extends React.Component{
     this.AddComment=this.AddComment.bind(this);
     this.CreateLoginPopup = this.CreateLoginPopup.bind(this);
     this.CreateSignUpPopup = this.CreateSignUpPopup.bind(this);
-    this.InteractWithPost = this.InteractWithPost.bind(this);
+    this.DoBold = this.DoBold.bind(this);
+    this.SubmitNewPost = this.SubmitNewPost.bind(this);
   }
 
 
@@ -120,13 +124,25 @@ class App extends React.Component{
 
   //likes and dislikes
   MakePostInteraction(postID, isLike){
-    this.state.client.SendPostLike(postID, isLike);
+    let interactions = this.state.postInteractions[postID]
+    if (!(isLike ? interactions.liked : interactions.disliked)){
+      this.state.client.SendPostLike(postID, isLike);
+      
+      if (isLike){
+        this.state.postInteractions[postID].liked = true;
+      }
+      else{
+        this.state.postInteractions[postID].disliked = true;
+      }
+    }
   }
 
 
   SubmitNewPost(user,postID,postTitle,message){
     let newPost={ posterUserName:user, postID:postID, title:postTitle, text:message, likes:0, dislikes:0};
     this.setState({allPosts: (prevPosts=>[newPost,...prevPosts])(this.state.allPosts)});
+    //this.setState({postInteractions: (interactions => {interactions[postID] = {liked: false, disliked: false};})(this.state.postInteractions)})
+    this.state.postInteractions[postID] = {liked: false, disliked: false};
   }
 
   // SubmitNewComment(postTitle,user,message){ //not functional
@@ -143,14 +159,6 @@ class App extends React.Component{
     }
     //this.setState({allPosts: this.state.allPosts});
   }
-
-
-  // SubmitLikePost(postID){ //outdated
-  //   let index = this.state.allPosts.findIndex((post)=>post.postID==postID);
-  //   let newObj = this.state.allPosts[index];
-  //   newObj.likes += 1
-  //   this.setState({allPosts: this.state.allPosts.with(index, newObj)});
-  // }
 
   SubmitPostInteraction(postID, isLike){
     if (isLike){
@@ -172,8 +180,13 @@ class App extends React.Component{
       <h4>{props.posterUserName}</h4>
       <h3>{props.title}</h3>
       <p>{props.text}</p>
-      <button onClick={()=>{this.MakePostInteraction(props.key, true)}}>likes: {props.likes}</button> | <button onClick={()=>{this.MakePostInteraction(props.key, false)}}>dislikes: {props.dislikes}</button>
+      <button onClick={()=>{this.MakePostInteraction(props.key, true)}}>{this.DoBold(`Likes: ${props.likes}`, props.key, true)}</button> | <button onClick={()=>{this.MakePostInteraction(props.key, false)}}>{this.DoBold(`dislikes: ${props.dislikes}`, props.key, false)}</button>
     </div>);
+  }
+
+  DoBold(text, postID, intrctn){
+    let intrctns = this.state.postInteractions[postID]; 
+    return (intrctn ? intrctns.liked : intrctns.disliked) ? <b>{text}</b> : <div>{text}</div>;
   }
 
   render(){
@@ -210,7 +223,6 @@ function Comment(props){
     </div>
   )
 }
-
 
 
 export default App;
