3c95b60e79d562264311335b67cd0c7325e14806
Hargaflarb <40022389+Hargaflarb@users.noreply.github.com>
2026-03-30 12:25:44 +0200

    Revert "Comments   emma"

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ class App extends React.Component{
       allPosts:[
         // TemplatePost(), TemplatePost(),TemplatePost(),TemplatePost(),TemplatePost()
       ], 
-      allComments:[],
+      allComments:[
+
+      ],
       postInteractions:[
 
       ],
@@ -34,64 +36,38 @@ class App extends React.Component{
   }
 
 
-  CreatePostPopup()
-  {
-    let postWindow=window.open("","newPostWindow","width=600,height=600 popup=true");
-    postWindow.document.body.innerHTML=("<div id='root'></div>");
-    postWindow.document.body.style.backgroundColor="gray";
-    postWindow.document.getElementById("root").style.height="100%";
-    postWindow.document.getElementById("root").style.width="100%";
-    const subRoot = ReactDOM.createRoot(postWindow.document.getElementById('root'));
-    subRoot.render(
-      <React.StrictMode>
-        <>
-          <h1>New post</h1>
-          <hr/>
-          <input type="text" id="titleTextbox" placeholder='New Post Title' style={{backgroundColor: 'light-gray'}} minLength={1} maxLength={100}></input>
-          <br/>
-          <textarea id="contentTextbox" placeholder='Write your post here' style={{backgroundColor: 'light-gray', height:'70%',width:'98%', alignSelf:'center'}} minLength={1} maxLength={2000}></textarea>
-          <br/>
-          <button id="submitPostBtn" onClick={()=>this.ExtractText(postWindow.document)} style={{height:'8%', width:'20%', float:'right', fontSize:'100%'}}>Post</button>
-        </>
-      </React.StrictMode>
-    );
-  }
+
+
   ViewComments(post){
     let hasComments = this.state.allComments[post.postID] == undefined;
     if (hasComments){
       this.state.client.RequestCommentHistory(post.postID);
     }
-    
-    console.log(post.title+" was looked at by (username)");
-    let commentWindow=window.open("","commentsWndow","width=700,height=500 popup=true")
+
+    let commentWindow=window.open("","commentsWndow","width=400,height=200 popup=true");
     commentWindow.document.body.innerHTML=("<div id='root'></div>");
-
-    commentWindow.document.getElementById("root").style.height="100%";
-    commentWindow.document.getElementById("root").style.width="100%";
-    commentWindow.document.body.style.backgroundColor="gray";
-
-  const subRoot = ReactDOM.createRoot(commentWindow.document.getElementById('root'));
+    const subRoot = ReactDOM.createRoot(commentWindow.document.getElementById('root'));
     subRoot.render(
       <React.StrictMode>
         <>
-        <div style={{backgroundColor: 'lightgray', position: 'sticky', padding:'15px', top:'0', wordWrap:'break-word'}}>
-          <h4>{post.posterUserName}</h4>
-          <h3>{post.title}</h3>
-          <p>{post.text}</p>
-        </div>
-        <br/>
-        <textarea id='commentTextbox' placeholder='Comment...' style={{position:'sticky', top:'100', width: "100%"}} minLength={1} maxLength={500}></textarea>
-        <button onClick={()=>this.ExtractCommentText(commentWindow.document,post.postID)} style={{position:'sticky', top:'100'}}>Submit</button>
-        <div style={{overflow: 'scroll'}}>
-        {
-          this.state.allComments.filter(comment=>comment.postID==post.postID).map((comment)=>
-            this.Comment({
-              key:comment.postID,
-              posterUserName:post.posterUserName,
-              text:comment.text,
-              likes:comment.likes,
-              dislikes:comment.dislikes,
-          }))} : "This post has no comments."}
+        <div>
+        <h4>{post.posterUserName}</h4>
+      <h3>{post.title}</h3>
+      <p>{post.text}</p>
+      </div>
+        <textarea id='commentTextbox' placeholder='Comment...'></textarea>
+        <button onClick={()=>this.ExtractCommentText(commentWindow.document,post.postID)}>Submit</button>
+        <div>{
+          // this.state.allComments.filter(comment=>comment.postID == post.postID).map((comment)=>
+          (this.state.allComments[post.postID] != undefined) ? this.state.allComments[post.postID].map((comment)=>
+          Comment({
+            commentID:comment.commentID,
+            postID:comment.postID,
+            posterUserName:comment.posterUserName,
+            text:comment.text,
+            likes:comment.likes,
+            dislikes:comment.dislikes,
+          })) : "This post has no comments."}
         </div>
           
         </>
@@ -99,16 +75,11 @@ class App extends React.Component{
     );
   }
 
-
-    ExtractCommentText(postDocument,postID){
+  ExtractCommentText(postDocument,postID){
     let text=postDocument.getElementById("commentTextbox").value;
-    postDocument.getElementById("commentTextbox").value="";
-    console.log("button pressed!");
-    if(text.length<1){
-      window.open("","commentsWndow").alert("You need to put something in the comment field before you can comment on this post.")
-    }
-    else this.AddComment(text,post);
-    }
+    this.state.client.SendComment(postID, text);
+  }
+
   CreatePostPopup()
   {
     let postWindow=window.open("","newPostWindow","width=600,height=600 popup=true");
@@ -135,26 +106,14 @@ class App extends React.Component{
   ExtractText(postDocument){
     let title=postDocument.getElementById("titleTextbox").value;
     let text=postDocument.getElementById("contentTextbox").value;
-    if(title.length<1){
-      window.open("","newPostWindow").alert("Your post must have a title before it can be submitted.")
-    }
-    else if(text.length<1){
-      window.open("","newPostWindow").alert("Your post must have text before it can be submitted.")
-    }
-    else{
     window.open("","newPostWindow").close();
     console.log("button pressed!");
     this.state.client.SendPost(title, text);
-    }
   }
 
   CreateLoginPopup(){
     let logInWindow=window.open("","LogInWindow","width=400,height=200 popup=true");
     logInWindow.document.body.innerHTML=("<div id='root'></div>");
-
-    logInWindow.document.getElementById("root").style.height="100%";
-    logInWindow.document.getElementById("root").style.width="100%";
-    logInWindow.document.body.style.backgroundColor="gray";
 
     const subRoot = ReactDOM.createRoot(logInWindow.document.getElementById('root'));
     subRoot.render(
@@ -162,9 +121,9 @@ class App extends React.Component{
         <>
           <h1>Log in</h1>
           <hr/>
-          <input type="text" id="usernameTextbox" placeholder='Username' minLength={1} maxLength={20}></input>
+          <input type="text" id="usernameTextbox" placeholder='Username'></input>
           <br/>
-          <input type="text" id="passwordTextbox" placeholder='Password' minLength={1} maxLength={100}></input>
+          <input type="text" id="passwordTextbox" placeholder='Password'></input>
       
           <button id="submitLoginBtn" onClick={()=>this.ExtractLogInDetails(logInWindow.document)}>Log In</button>
         </>
@@ -175,26 +134,14 @@ class App extends React.Component{
   ExtractLogInDetails(logInDocument){
     let username = logInDocument.getElementById("usernameTextbox").value;
     let password = logInDocument.getElementById("passwordTextbox").value;
-    if(username.length<1){
-      window.open("","LogInWindow").alert("Please enter your username.")
-    }
-    else if(password.length<1){
-      window.open("","LogInWindow").alert("Please enter your password.")
-    }
-    else{
-      window.open("","LogInWindow").close();
-      console.log("button pressed!");
-      this.state.client.RequestLogIn(username, password);
-    }
+    window.open("","LogInWindow").close();
+    console.log("button pressed!");
+    this.state.client.RequestLogIn(username, password);
   }
 
   CreateSignUpPopup(){
     let signUpWindow=window.open("","SignUpWindow","width=400,height=200 popup=true");
     signUpWindow.document.body.innerHTML=("<div id='root'></div>");
-
-    signUpWindow.document.getElementById("root").style.height="100%";
-    signUpWindow.document.getElementById("root").style.width="100%";
-    signUpWindow.document.body.style.backgroundColor="gray";
 
     const subRoot = ReactDOM.createRoot(signUpWindow.document.getElementById('root'));
     subRoot.render(
@@ -202,9 +149,9 @@ class App extends React.Component{
         <>
           <h1>Sign Up</h1>
           <hr/>
-          <input type="text" id="usernameTextbox" placeholder='Username' minLength={1} maxLength={20}></input>
+          <input type="text" id="usernameTextbox" placeholder='Username'></input>
           <br/>
-          <input type="text" id="passwordTextbox" placeholder='Password' minLength={1} maxLength={100}></input>
+          <input type="text" id="passwordTextbox" placeholder='Password'></input>
       
           <button id="submitSignUpBtn" onClick={()=>this.ExtractSignUpDetails(signUpWindow.document)}>Sign Up</button>
         </>
@@ -215,18 +162,9 @@ class App extends React.Component{
   ExtractSignUpDetails(signUpWindow){
     let username = signUpWindow.getElementById("usernameTextbox").value;
     let password = signUpWindow.getElementById("passwordTextbox").value;
-    if(username.length<1)
-      {
-        window.open("","SignUpWindow").alert("Please enter a new username");
-      }
-    else if(password.length<1){
-      window.open("","SignUpWindow").alert("Please enter a new password");
-    }
-    else{
     window.open("","SignUpWindow").close();
     console.log("button pressed!");
     this.state.client.SendSignUp(username, password);
-    }
   }
 
 
@@ -320,14 +258,6 @@ class App extends React.Component{
     this.setState({allPosts: this.state.allPosts});
   }
 
-Comment(props){
-  return(
-    <div className='comment' style={{backgroundColor:'lightgray', padding:'5px', margin:'5px'}}>
-      {/* <h5>{props.posterUserName}</h5> */}
-      <p style={{wordWrap:"break-word",overflow: "scroll", maxHeight:"150px"}}>{props.text}</p>
-    </div>
-  )
-}
   
   
 
@@ -359,6 +289,8 @@ Comment(props){
       // window.document.getElementsByClassName("newCommentField:hover").style.backgroundColor = "#ffffff";
     }
   }
+
+
 
   Post(props){
     return(
@@ -403,6 +335,17 @@ Comment(props){
   }
 }
 
+
+
+function Comment(props){
+  return(
+    <div className='comment'>
+      <h5>{props.posterUserName}</h5>
+      <p>{props.text}</p>
+      <button onClick={MakeCommentInteraction(props.commentID, true)}>{`Likes: ${props.likes}`}</button> | <button onClick={MakeCommentInteraction(props.commentID, false)}>{`dislikes: ${props.dislikes}`}</button>
+    </div>
+  )
+}
 
 function changeBGColor(coll, color){
 
