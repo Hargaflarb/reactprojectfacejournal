5a5c0f31971cb81f42dbaebf285d5fe647b89117
Hvemvilhvad <malthewbirch@gmail.com>
2026-03-31 14:31:38 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ let client = new WSClient()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1 onClick={()=>client.SendPost("ayyy, i'm post 'ere!!!")}>HI BITCHES</h1>
     <App client={client}/>
   </React.StrictMode>
 );
